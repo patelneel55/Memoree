8be545478802b69fe16a0e6a242c800f7c4f8726
Neel Patel <patelneel55@gmail.com>
2020-07-31 11:02:24 -0500

    Build video intelligencece request to analyze video

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -23,11 +23,62 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
+const videoIntel = require('@google-cloud/video-intelligence');
 require('dotenv').config()
 
 admin.initializeApp();
 
+
+/**
+ * Uses the GCloud Video Intelligence API to analyze
+ * video added to the GCloud Storage Bucket
+ *
+ * @param {*} bucketObject The video file added to GCloud storage
+ */
+async function runVideoAnalyzer(bucketObject) {
+
+	let filePath = bucketObject.name;
+	let jsonPath = bucketObject.name.split('.')[0] + '.json'
+
+	let request = {
+		inputUri: `gs://${bucketObject.bucket}/${bucketObject.name}`,
+		outputUri: `gs://${process.env.VIDEO_JSON_BUCKET}/${jsonPath}`,
+		features: [
+			"LABEL_DETECTION",
+			"SHOT_CHANGE_DETECTION",
+			"EXPLICIT_CONTENT_DETECTION",
+			"FACE_DETECTION",
+			"SPEECH_TRANSCRIPTION",
+			"TEXT_DETECTION",
+			"OBJECT_TRACKING",
+			"LOGO_RECOGNITION",
+			"PERSON_DETECTION"
+		],
+		videoContext: {
+			speechTranscriptionConfig: {
+				languageCode: "en-US",
+				enableAutomaticPunctuation: true
+			},
+			faceDetectionConfig: {
+				includeBoundingBoxes: true,
+				includeAttributes: true,
+			},
+			textDetectionConfig: {
+				languageHints: ["en-US", "hi-IN", "gu-IN", "ta-IN"]
+			},
+			personDetectionConfig: {
+				includeBoundingBoxes: true,
+				includePoseLandmarks: true,
+				includeAttributes: true
+			}
+		}
+	}
+
+	const videoClient = videoIntel.VideoIntelligenceServiceClient()
+
+	const [operation] = await videoClient.annotateVideo(request);
+	console.log("Video annotation initatied: ", operation)
+}
 
 
 // The following functions are triggered when a new entity is added or
@@ -40,4 +91,7 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 exports.analyzeVideo = functions
 	.storage(process.env.VIDEO_BUCKET)
 	.object()
-	.onFinalize(async (object) => {}) // Process video using Video Intelligence API and extract preview image
+	.onFinalize(async (object) => {
+		await Promise.all([runVideoAnalyzer(object)]);
+
+	})
