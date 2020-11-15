cc11b28d4a0e038bba24e1304f1483c6ebedcea2
Neel Patel <patelneel55@gmail.com>
2020-11-14 19:23:28 -0500

    Fix line endings

--- frontend/memoree_client/lib/main.dart
+++ frontend/memoree_client/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:memoree_client/search.dart';
+import 'package:memoree_client/themes.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,17 +11,17 @@ class MyApp extends StatelessWidget {
     const String _app_title = "Memoree";
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: _app_title,
-      theme: ThemeData(
-        fontFamily: "OpenSans",
-        primaryColor: Colors.white
-      ),
+      theme: AppTheme.lightTheme,
       home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
+  final String appTitle = "Memoree";
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -28,19 +30,52 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Memoree"),
-        elevation: 2,
-        actions: <Widget>[
-          // searchBar(),
-
-        ]
-      ),
+      appBar: appBar(),
+      body: Container(),
     );
   }
 
-  Widget searchBar()
+  AppBar appBar()
   {
-
+    return AppBar(
+      toolbarHeight: 75,
+      elevation: 1,
+      automaticallyImplyLeading: false,
+      flexibleSpace: Container(),
+      centerTitle: true,
+        title: Row(
+          children: <Widget>[
+            Text("Memoree"),
+            Flexible(
+              flex: 5,
+              child: Container(
+                padding: const EdgeInsets.all(100.0),
+                child: SearchWidget(),
+              )
+            ),
+            Expanded(flex: 2, child: Container())
+          ],
+        ),
+        actions: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(right: 10.0, left: 10.0),
+            child: IconButton(
+              icon: const Icon(Icons.cloud_upload_outlined),
+              tooltip: "Upload",
+              splashRadius: 25,
+              onPressed: () => {}
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(right: 10.0, left: 10.0),
+            child: IconButton(
+              icon: const Icon(Icons.settings_outlined),
+              tooltip: "Settings",
+              splashRadius: 25,
+              onPressed: () => {}
+            ),
+          ),
+        ]
+      );
   }
 }
